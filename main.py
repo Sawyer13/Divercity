188d9e97b8fff3893433296968587c2e9d2ea28b
Axel Junes <axeljunes@Axels-MacBook-Pro.local>
2017-12-02 19:08:06 +0100

    First Deployment

--- main.py
+++ main.py
@@ -38,5 +38,11 @@ def add_restroom():
         return render_template("form.html", r=restroom)
 
 
+# About page
+@app.route("/about", methods=["GET"])
+def about():
+    if request.method == "GET":
+        return render_template("about.html")
+
 if __name__ == "__main__":
     app.run()
